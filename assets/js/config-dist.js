3fa0808cdd9121b6608b4ea7823d8416d2ca042e
Chris Grindstaff <chris@gstaff.org>
2013-04-19 15:06:28 -0400

    Reorder config to highlight api

--- assets/js/config-dist.js
+++ assets/js/config-dist.js
@@ -1,10 +1,11 @@
 (function(d){
   config = {
+    api:'http://example.com/tt-rss/api/',
+
     // FEED_LIMIT does not really work with unread only since the unread check
     // is done in code after the db query that includes a limit. there will be missed
     // results
     ARTICLE_LIMIT:  30,
-    api:'http://example.com/tt-rss/api/',
     
     // Auto refresh every x number of minutes. Be nice to your server :)
     // Turn auto refresh off -1
